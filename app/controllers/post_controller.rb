43f3bed37b677d3e357c6af4c2b6385494feb80e
ddbrennan <ddbren@gmail.com>
2017-12-13 15:40:24 -0500

    blog post app working

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,41 @@
+class PostController < ApplicationController
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    Post.create(params)
+    redirect '/posts'
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.update(name: params[:name], content: params[:content])
+
+    redirect "/posts/#{params[:id]}"
+  end
+
+  delete '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    erb :delete
+  end
+
+end
